16066865ea4d983855e02c225367d4c594a28139
Tung Pham <tpv.142@gmail.com>
2019-02-19 13:40:07 -0600

    finalize search

--- ted.py
+++ ted.py
@@ -7,13 +7,32 @@ result = []
 
 @app.route("/")
 def home():
-	return render_template('index.html', data = ted.tedData, result = result)
+	return render_template('index.html', data = ted.tedData, result = result, search = False)
 
 @app.route("/", methods=['POST'])
 def search_form():
 	search = request.form['search']
 	result = ted.search(search)
-	return render_template('index.html', data = ted.tedData, result = result, scroll='found')
+	documents = []
+	embed_url = []
+
+	if(len(result) < 6):
+		length = len(result)
+	else:
+		length = 6
+
+	for i in range(length):
+		documents.append(result[i])
+
+	title = list(ted.tedData['title'][documents])
+	url = list(ted.tedData['url'][documents])
+	description = list(ted.tedData['description'][documents])
+	author = list(ted.tedData['main_speaker'][documents])
+
+	for link in url:
+		embed_url.append(link.replace("www", "embed", 1))
+
+	return render_template('index.html', data = ted.tedData, result = documents, scroll='found', title = title, url = embed_url, description = description, author = author, search = True)
 
 if __name__ == '__main__':
 	app.run(debug=True)
